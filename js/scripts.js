b3b7ba26fe16406701a9018a7cf83554980d103a
RyanTarnowski <ryan.tarnowski@gmail.com>
2025-01-14 12:56:26 -0700

    Add dropdown menu, tweak js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
 function showProject(id) {
     var x = document.getElementById(id);
-    x.style.display = x.style.display === "none" ? "block" : "none"
+    x.style.display = x.style.display !== "block" ? "block" : "none"
 };
