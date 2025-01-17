661952635c2d1685f8b1e8d83baf3d27e225bd16
RyanTarnowski <ryan.tarnowski@gmail.com>
2025-01-16 17:04:55 -0700

    Move index to root.

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,16 @@ function changeTheme(theme){
 
     switch (theme) {
         case 'light':
-            styleSheet = '../css/light.css'
+            styleSheet = './css/light.css'
         break;
         case 'dark':
-            styleSheet = '../css/dark.css'
+            styleSheet = './css/dark.css'
         break;
         case 'terminal':
-            styleSheet = '../css/terminal.css'
+            styleSheet = './css/terminal.css'
         break;
         default:
-            styleSheet = '../css/light.css'
+            styleSheet = './css/light.css'
         break;
     }
 
@@ -27,27 +27,27 @@ function changeTheme(theme){
 
 function changeIcons(theme) {
     if (theme =='terminal') {
-        document.getElementById("vb").setAttribute("src", "../images/TerminalTheme/icons8-vb-50.png");
-        document.getElementById("c").setAttribute("src", "../images/TerminalTheme/icons8-c-50.png");
-        document.getElementById("asp").setAttribute("src", "../images/TerminalTheme/icons8-asp-52.png");
-        document.getElementById("sql").setAttribute("src", "../images/TerminalTheme/icons8-sql-50.png");
-        document.getElementById("html").setAttribute("src", "../images/TerminalTheme/icons8-html-50.png");
-        document.getElementById("css").setAttribute("src", "../images/TerminalTheme/icons8-css-50.png");
-        document.getElementById("js").setAttribute("src", "../images/TerminalTheme/icons8-js-50.png");
-        document.getElementById("ang").setAttribute("src", "../images/TerminalTheme/icons8-angular-50.png");
-        document.getElementById("linkedIn").setAttribute("src", "../images/TerminalTheme/icons8-linked-in-50.png");
-        document.getElementById("github").setAttribute("src", "../images/TerminalTheme/icons8-github-50.png");
+        document.getElementById("vb").setAttribute("src", "./images/TerminalTheme/icons8-vb-50.png");
+        document.getElementById("c").setAttribute("src", "./images/TerminalTheme/icons8-c-50.png");
+        document.getElementById("asp").setAttribute("src", "./images/TerminalTheme/icons8-asp-52.png");
+        document.getElementById("sql").setAttribute("src", "./images/TerminalTheme/icons8-sql-50.png");
+        document.getElementById("html").setAttribute("src", "./images/TerminalTheme/icons8-html-50.png");
+        document.getElementById("css").setAttribute("src", "./images/TerminalTheme/icons8-css-50.png");
+        document.getElementById("js").setAttribute("src", "./images/TerminalTheme/icons8-js-50.png");
+        document.getElementById("ang").setAttribute("src", "./images/TerminalTheme/icons8-angular-50.png");
+        document.getElementById("linkedIn").setAttribute("src", "./images/TerminalTheme/icons8-linked-in-50.png");
+        document.getElementById("github").setAttribute("src", "./images/TerminalTheme/icons8-github-50.png");
     }
     else {
-        document.getElementById("vb").setAttribute("src", "../images/icons8-vb-100.png");
-        document.getElementById("c").setAttribute("src", "../images/icons8-c-100.png");
-        document.getElementById("asp").setAttribute("src", "../images/icons8-asp-100.png");
-        document.getElementById("sql").setAttribute("src", "../images/icons8-sql-96.png");
-        document.getElementById("html").setAttribute("src", "../images/icons8-html-100.png");
-        document.getElementById("css").setAttribute("src", "../images/icons8-css3-100.png");
-        document.getElementById("js").setAttribute("src", "../images/icons8-javascript-100.png");
-        document.getElementById("ang").setAttribute("src", "../images/icons8-angular-100.png");
-        document.getElementById("linkedIn").setAttribute("src", "../images/icons8-linked-in-100.png");
-        document.getElementById("github").setAttribute("src", "../images/icons8-github-100.png");
+        document.getElementById("vb").setAttribute("src", "./images/icons8-vb-100.png");
+        document.getElementById("c").setAttribute("src", "./images/icons8-c-100.png");
+        document.getElementById("asp").setAttribute("src", "./images/icons8-asp-100.png");
+        document.getElementById("sql").setAttribute("src", "./images/icons8-sql-96.png");
+        document.getElementById("html").setAttribute("src", "./images/icons8-html-100.png");
+        document.getElementById("css").setAttribute("src", "./images/icons8-css3-100.png");
+        document.getElementById("js").setAttribute("src", "./images/icons8-javascript-100.png");
+        document.getElementById("ang").setAttribute("src", "./images/icons8-angular-100.png");
+        document.getElementById("linkedIn").setAttribute("src", "./images/icons8-linked-in-100.png");
+        document.getElementById("github").setAttribute("src", "./images/icons8-github-100.png");
     }
 }
